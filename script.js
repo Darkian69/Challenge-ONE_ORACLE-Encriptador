557684bfd48efc77b4b0999a21e40deadfbd774a
Darkian69 <j.aguirre2@utp.edu.co>
2022-09-18 18:08:16 -0500

    Se agregaron las funciones encriptar, desencrp itar y copiar con sus respectivos botones

--- script.js
+++ script.js
@@ -6,17 +6,37 @@ function capturar() {
 function encriptar(){
     var texto = capturar();
     var resultado;
-    resultado = texto.replace(/a/gi,"xd")
+    resultado = texto
     .replace(/e/g, "enter")
     .replace(/i/g, "imes")
+    .replace(/a/gi,"ai")
     .replace(/o/g, "ober")
     .replace(/u/g, "ufat");
     console.log(resultado);
-    var xd = document.getElementById("text-output");
-    xd.innerHTML = resultado;
+    var salida = document.getElementById("text-output");
+    salida.innerHTML = resultado;
 }
 
-//el pepe pedro piedra
-//enterl penterpenter penterdrober pimesenterdrai
-//enterl pepe pedrober pimesedrai
-//enterl penterpenter penterdrober pimesenterdraimes
+function desencriptar() {
+    var texto = capturar();
+    var resultado;
+    resultado = texto
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/gi,"a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+    console.log(resultado);
+    var salida = document.getElementById("text-output");
+    salida.innerHTML = resultado;
+}
+
+function updateClipboard() {
+    var salida = document.getElementById("text-output").textContent;
+    navigator.clipboard.writeText(salida).then(() => {
+      console.log("success");
+    }, () => {
+      /* clipboard write failed */
+      console.log("failed");
+    });
+  }
