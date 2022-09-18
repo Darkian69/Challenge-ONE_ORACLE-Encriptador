f4d164484183e3513cf31a53b2077644546e7e77
Darkian69 <j.aguirre2@utp.edu.co>
2022-09-18 17:10:52 -0500

    Se agrego la funcion capturar y encriptar, se relaciono a un botn

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function capturar() {
     var texto = document.getElementById("entrada-texto").value;
     return texto;
 }
+
+function encriptar(){
+    var texto = capturar();
+    var resultado;
+    resultado = texto.replace(/a/gi,"xd")
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+    console.log(resultado);
+    var xd = document.getElementById("text-output");
+    xd.innerHTML = resultado;
+}
+
+//el pepe pedro piedra
+//enterl penterpenter penterdrober pimesenterdrai
+//enterl pepe pedrober pimesedrai
+//enterl penterpenter penterdrober pimesenterdraimes
